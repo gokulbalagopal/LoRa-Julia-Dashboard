afaf3694b00d8027e28468d689549e01663a00fb
gokulbalagopal <balagopalgokul@gmail.com>
2021-09-23 15:40:42 -0500

    updated

--- GroundVehicleDashBoard.jl
+++ GroundVehicleDashBoard.jl
@@ -1,28 +1,73 @@
-using CSV, DataFrames, Dates,Statistics, Dash, DashHtmlComponents, DashCoreComponents, PlotlyJS, DataStructures,JSONTables
-ground_vehicle_path = "/home/teamlary/mintsData/referenceMQTT/001e0610c2e7/"
+using CSV, DataFrames, Dates,Statistics, Dash, DashHtmlComponents, DashCoreComponents, PlotlyJS, DataStructures,JSON3
+global gnd_vehicle_data = DataFrame()
+#deque_o3 = Deque{Float64}()
+#deque_dt = Deque{DateTime}()
 
-#Filtering the json files
-ground_vehicle_sensor_list = filter!(s->occursin(r".json", s),readdir(ground_vehicle_path))
-ground_vehicle_sensor_path_list = ground_vehicle_path .* ground_vehicle_sensor_list
+function gnd_vehicle_data_update()
+    ground_vehicle_path ="/home/teamlary/mintsData/referenceMQTT/001e0610c2e7/"
+    #Filtering the json files
+    ground_vehicle_sensor_list = filter!(s->occursin(r".json", s),readdir(ground_vehicle_path))
+    sensor_name = map((x)-> replace(x, r".json"=>""),ground_vehicle_sensor_list)
 
-#Creating a single DataFrame with all the json files
-df_ls = map((x)-> DataFrame(JSON3.read(read(x))),ground_vehicle_sensor_path_list)
-df_combined = reduce(vcat, df_ls, cols=:union)
+    ground_vehicle_sensor_path_list = ground_vehicle_path .* ground_vehicle_sensor_list
 
-
-
-#= need to handle this with prometheus / sql  and make a single data frame with all the sensors combined
-app = dash()
-trace1 = scatter(;x = r[!,"date_time_rounded"], y = r[!,"NH3_mean"],mode= "markers + lines", marker_size = 5, marker_color = :green)
-p1 = plot([trace1])
-app.layout = html_div() do
-    html_h1("Finally"),
-    html_div("Its working"),
-    dcc_graph(
-        id="figure",
-        figure=p1,
-    )
+    #Creating a single DataFrame with all the json files
+    df_ls = map((x)-> DataFrame(JSON3.read(read(x))),ground_vehicle_sensor_path_list)
+    df_combined = reduce(vcat, df_ls, cols=:union)
+    df_combined = insertcols!(df_combined, 2, :sensors => sensor_name, makeunique=true)
+    return append!(gnd_vehicle_data,df_combined)
+    
 end
 
-run_server(app, "0.0.0.0", debug=true)
-=#
+function update_o3_graph()
+    df = gnd_vehicle_data_update()
+    if(nrow(df) >= 100000)
+        delete!(df, 1:8)
+    end
+    df_ozone = dropmissing(df, :ozone)
+    data = scatter(;
+                x = df_ozone.dateTime,
+                y = df_ozone.ozone,
+                mode = "lines+markers",
+                name = "Ozone",
+                marker_color = :red,
+                marker_size = 5,
+                    )
+    
+    layout = Layout(;
+                title = "Ozone",
+                xaxis_title = "time",
+                yaxis_title = "O3",
+                    )
+    plot(data,layout)
+  
+
+end
+
+app = dash()
+
+app.layout = html_div() do
+
+            dcc_graph(
+                id="o3-live-graph",
+                figure = update_o3_graph()
+            ),
+
+            dcc_interval(
+                id="o3-graph-update",
+                interval = 100, 
+                n_intervals=0
+            )
+
+end
+
+
+
+callback!(app,
+            Output("o3-live-graph", "figure"),
+            Input("o3-graph-update", "n_intervals")) do n
+            update_o3_graph()
+
+end
+
+run_server(app, "0.0.0.0",debug = true)
--- ex.jl
+++ ex.jl
@@ -1,5 +1,8 @@
-using CSV, DataFrames, Dates,Statistics, Dash, DashHtmlComponents, DashCoreComponents,PlotlyJS
 
+using CSV, DataFrames, Dates,Statistics, Dash, DashHtmlComponents, DashCoreComponents, PlotlyJS
+
+
+#using TimeSeriesIO: TimeArray
 data_frame = CSV.read("//mnt//3fbf694d-d8e0-46c0-903d-69d994241206//mintsData//raw//b827eb52fc29//47cb5580002e004a//47cb5580002e004a_2021-06-17.csv",DataFrame)
 data_frame = select(data_frame,Not([:gpsTime,:id]))
 
@@ -28,29 +31,17 @@ function resampling_time_series_data(w,tf,df)
 
 end
 r =resampling_time_series_data("m",1,data_frame)
-sensors= Dict("dt" => r.date_time_rounded,"NH3" =>r.NH3_mean)
-data = contour(;
-x = sensors[:dt],
-y = sensor[:NH3],
-)
 
-layout = Layout(;
- title = "Particle Size Distribution",
- xaxis_title = "time",
- yaxis_title = "Particle Radius [μm]",
-
- )
-plot(data, layout)
 app = dash()
-#p1 = plot(r,x = r[!,"date_time_rounded"], y = r[!,"NH3_mean"],mode= "markers + lines", marker_size = 5, marker_color = :green)
-#p1 = plot([trace1])
+trace1 = scatter(;x = r[!,"date_time_rounded"], y = r[!,"NH3_mean"],mode= "markers + lines", marker_size = 5, marker_color = :green)
+p1 = plot([trace1])
 app.layout = html_div() do
     html_h1("Finally"),
     html_div("Its working"),
     dcc_graph(
         id="figure",
-        figure= [p1],
+        figure=p1,
     )
 end
 
-run_server(app, "0.0.0.0", debug=false)
+run_server(app, "0.0.0.0", debug=true)
